cd8c26570035c254fa8d649ac5b19263d58846a9
Breaker-93 <1094143240@qq.com>
2020-05-04 19:12:26 +0800

    feat: 添加“vuex”的状态管理、实现登录功能

--- src/assets/js/auth/index.js
+++ src/assets/js/auth/index.js
@@ -1,4 +1,5 @@
 const TokenKey = 'token'
+const sidebarStatusKey = 'sidebarStatus'
 
 export function getToken() {
   return localStorage.getItem(TokenKey) || sessionStorage.getItem(TokenKey)
@@ -10,4 +11,16 @@ export function setToken(token) {
 
 export function removeToken() {
   localStorage.removeItem(TokenKey) && sessionStorage.removeItem(TokenKey)
+}
+
+export function getSidebarStatus() {
+  return localStorage.getItem(sidebarStatusKey) || sessionStorage.getItem(sidebarStatusKey)
+}
+
+export function setSidebarStatus(status) {
+  return localStorage.setItem(sidebarStatusKey, status)
+}
+
+export function removeSidebarStatus() {
+  localStorage.removeItem(sidebarStatusKey) && sessionStorage.removeItem(sidebarStatusKey)
 }
