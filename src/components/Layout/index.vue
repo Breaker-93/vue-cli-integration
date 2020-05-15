7d1e5eb7cdfc1fc799b51691626947492cf36417
Breaker-93 <1094143240@qq.com>
2020-05-15 10:32:14 +0800

    feat: 实现登出功能

--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="app-wrapper">
-    
     <el-container>
       <el-aside :width="isCollapse ? 'auto' : '200px'">
         <div class="system-name">
           <i class="el-icon-box" :title="systemName"></i>
-          <span v-if="!isCollapse">&nbsp;{{systemName}}</span>
+          <span v-if="!isCollapse">&nbsp;{{ systemName }}</span>
         </div>
         <transition name="fade">
           <el-menu
@@ -16,14 +15,40 @@
             :text-color="menuStyle.menuText"
             :active-text-color="menuStyle.menuActiveText"
           >
-            <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path"/>
+            <sidebar-item
+              v-for="route in routes"
+              :key="route.path"
+              :item="route"
+              :base-path="route.path"
+            />
           </el-menu>
         </transition>
       </el-aside>
       <el-container>
         <el-header height="50px">
-          <hamburger :toggle-click="toggleSideBar" :is-active="isCollapse" class="hamburger-container"/>
-          <breadcrumb/>
+          <hamburger
+            :toggle-click="toggleSideBar"
+            :is-active="isCollapse"
+            class="hamburger-container"
+          />
+          <breadcrumb />
+          <el-dropdown class="avatar-container" trigger="click">
+            <div class="avatar-wrapper">
+              <img v-if="avatar" :src="avatar" class="user-avatar" />
+              <svg-icon v-else icon-class="avatar"></svg-icon>
+              <i class="el-icon-caret-bottom" />
+            </div>
+            <el-dropdown-menu slot="dropdown" class="user-dropdown">
+              <router-link class="inline-block" to="/">
+                <el-dropdown-item>
+                  首页
+                </el-dropdown-item>
+              </router-link>
+              <el-dropdown-item divided>
+                <span style="display:block;" @click="toLogout">登出</span>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
         </el-header>
         <el-main>
           <router-view></router-view>
@@ -35,35 +60,41 @@
 </template>
 
 <script>
-import menuStyle from '@assets/css/variable.styl'
-import Hamburger from '@comp/Hamburger'
-import Breadcrumb from '@comp/Breadcrumb'
-import SidebarItem from './SidebarItem'
+import menuStyle from "@assets/css/variable.styl";
+import Hamburger from "@comp/Hamburger";
+import Breadcrumb from "@comp/Breadcrumb";
+import SidebarItem from "./SidebarItem";
 export default {
   name: "Layout",
   components: {
     Hamburger,
     Breadcrumb,
-    SidebarItem
+    SidebarItem,
   },
   props: {},
   data() {
     return {
       isCollapse: false,
       menuStyle,
-      systemName: '生产管理系统'
-    }
+      systemName: "生产管理系统",
+      avatar: null,
+    };
   },
   created() {},
   mounted() {},
   methods: {
     toggleSideBar() {
-      this.isCollapse = !this.isCollapse
-    }
+      this.isCollapse = !this.isCollapse;
+    },
+    toLogout() {
+      this.$store.dispatch("FedLogOut").then(() => {
+        location.reload(); // 为了重新实例化vue-router对象 避免bug
+      });
+    },
   },
   computed: {
     routes() {
-      return this.$router.options.routes
+      return this.$router.options.routes;
     },
   },
   watch: {},
@@ -128,6 +159,38 @@ export default {
     line-height 60px
     text-align center
     background #F2F6FC
+  }
+  .avatar-container {
+    height: 50px;
+    display: inline-block;
+    position: absolute;
+    right: 35px;
+    .avatar-wrapper {
+      cursor: pointer;
+      margin-top: 5px;
+      position: relative;
+      line-height: initial;
+      .svg-icon {
+        width: 40px;
+        height: 40px;
+      }
+      .user-avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 10px;
+      }
+      .el-icon-caret-bottom {
+        position: absolute;
+        right: -20px;
+        top: 25px;
+        font-size: 12px;
+      }
+    }
+  }
+}
+.user-dropdown {
+  .inline-block {
+    text-decoration none
   }
 }
 </style>
